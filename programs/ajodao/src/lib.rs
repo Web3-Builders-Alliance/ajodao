5805a775d875a28bdc7b2eee404b1b714ded4f66
Joshua Ogbonna <devjaykes@gmail.com>
2023-09-17 16:12:41 +0100

    code: add context

--- programs/ajodao/src/lib.rs
+++ programs/ajodao/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 pub mod instructions;
 pub mod state;
 
-pub use instruction::*;
+pub use instructions::*;
 pub use state::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
@@ -11,4 +11,14 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod ajodao {
     use super::*;
+
+    pub fn create_pot(
+        ctx: Context<CreatePot>,
+        description: String,
+        name: String,
+        cycle: PotCycles,
+        created_at: String,
+    ) -> Result<()> {
+        instructions::create_pot(ctx, description, name, cycle, created_at)
+    }
 }
